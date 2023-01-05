ad93efc5eb8717ade2cb69ed33fd31229d410603
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2023-01-05 12:18:03 +0530

    Create AirplaneSeatAssignmentProbability.java

---
+++ Microsoft/AirplaneSeatAssignmentProbability.java
@@ -0,0 +1,8 @@
+// link- https://leetcode.com/problems/airplane-seat-assignment-probability/
+
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        if(n==1) return 1;
+        return 0.5;
+    }
+}
