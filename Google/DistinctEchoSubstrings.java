aeaf056de1aebc006b8f695e6c2bf0eb7d4563e2
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2023-01-22 17:59:49 +0530

    Update DistinctEchoSubstrings.java

--- Google/DistinctEchoSubstrings.java
+++ Google/DistinctEchoSubstrings.java
@@ -25,53 +25,57 @@ class Solution {
 
 // Rolling hash Algo
 
+/*
+
+This solution uses a two-pointer approach where it starts with substring of length 1 and then iterates over all substring of that length
+ and check for the first and last character of substring if they are same or not. If they are same it increases the counter, if counter 
+becomes equal to the length of substring then it calculates hash value of substring and add it to the set and decrease the counter by 1.
+
+This solution has a Time complexity of O(n^2) which is better than the previous solutions because it iterates over all the substring of length
+ n/2 and also it uses a hashset which has a time complexity of O(1) for adding an element.
+
+*/
+
 class Solution {
-    long hash[];
-    long pow[];
-    long r=256;
-    long mod=(long)Math.pow(10,9)+7;
-    public void process(String s,int n)
-    {
-        hash=new long[n];
-        pow=new long[n];
-        pow[0]=1;
-        for(int i=1;i<n;i++)
-        {
-            hash[i]=(hash[i-1]*r+s.charAt(i))%mod;
-            pow[i]=(pow[i-1]*r)%mod;
+    private long hash[];
+    private long power[];
+    private long base = 256;
+    private long mod = (long)Math.pow(10,9) + 7;
+    
+    private void processString(String text, int textLength) {
+        hash = new long[textLength];
+        power = new long[textLength];
+        power[0] = 1;
+        for(int i = 1; i < textLength; i++) {
+            hash[i] = (hash[i-1] * base + text.charAt(i)) % mod;
+            power[i] = (power[i-1] * base) % mod;
         }
     }
-    public long calc(int l,int r)
-    {
-        long hashValue=(hash[r]-hash[l]*pow[r-l]%mod+mod)%mod;
+    
+    private long calculateHash(int left, int right) {
+        long hashValue = (hash[right] - hash[left] * power[right - left] % mod + mod) % mod;
         return hashValue;
     }
+    
     public int distinctEchoSubstrings(String text) {
-        int n=text.length();
-        process(text,n);
-        HashSet<Long> hset=new HashSet<>();
-        for(int len=1;len<=n/2;len++)
-        {
-            int c=0;
-            for(int l=0,r=len;r<n;l++,r++)
-            {
-                if(text.charAt(l)==text.charAt(r))
-                {
-                   c++; 
+        int textLength = text.length();
+        processString(text, textLength);
+        Set<Long> uniqueHashes = new HashSet<>();
+        for(int length = 1; length <= textLength/2; length++) {
+            int count = 0;
+            for(int left = 0, right = length; right < textLength; left++, right++) {
+                if(text.charAt(left) == text.charAt(right)) {
+                    count++; 
+                } else {
+                    count = 0;
                 }
-                else
-                {
-                    c=0;
-                }
-                if(c==len)
-                {
-                    long hv=calc(l,r);
-                    hset.add(hv);
-                    c--;
+                if(count == length) {
+                    long hashValue = calculateHash(left, right);
+                    uniqueHashes.add(hashValue);
+                    count--;
                 }
             }
         }
-        return hset.size();
-        
+        return uniqueHashes.size();
     }
 }
