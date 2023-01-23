f6e0f6967b021c2609523989117bf5593672711f
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2023-01-23 10:07:33 +0530

    Update KDivisibleElementsSubarrays.java

--- Google/KDivisibleElementsSubarrays.java
+++ Google/KDivisibleElementsSubarrays.java
@@ -80,57 +80,57 @@ class Solution {
 
 class Solution {
     private static class Node {
-        private int val;
-        private boolean isEnd;
-        private Node[] children;
+		private int val;
+		private boolean isEnd;
+		private Node[] children;
 
-        public Node(int val) {
-            this.val = val;
-            this.isEnd = false;
-            this.children = new Node[201];
-        }
-    }
+		public Node(int val) {
+			this.val = val;
+			this.isEnd = false;
+			this.children = new Node[201];
+		}
+	}
 
-    private int totalCount = 0;
-    private Node root = new Node(0);
+	private int totalCount = 0;
+	private Node root = new Node(0);
+	
+	private void insert(int[] nums, int left, int right) {
+		Node curr = this.root;
+		for (int i = left; i <= right; i++) {
+			int childIdx = nums[i];
+			if (curr.children[childIdx] == null) {
+				curr.children[childIdx] = new Node(childIdx);
+			}
+			curr = curr.children[childIdx];
+			if (!curr.isEnd) {
+				totalCount++;
+				curr.isEnd = true;
+			}
+		}
+	}
 
-    private void insert(int[] nums, int left, int right) {
-        Node curr = this.root;
-        for (int i = left; i <= right; i++) {
-            int childIdx = nums[i];
-            if (curr.children[childIdx] == null) {
-                curr.children[childIdx] = new Node(childIdx);
-            }
-            curr = curr.children[childIdx];
-            if (!curr.isEnd) {
-                totalCount++;
-                curr.isEnd = true;
-            }
-        }
-    }
-
-    public int kDivisibleSubarrays(int[] nums, int k, int p) {
-        int count = 0;
-        int n = nums.length;
-        int left = 0;
-        int right = 0;
-        while (left < n) {
-            while (right < n) {
-                if (nums[right] % p == 0) {
-                    count++;
-                }
-                if (count == k + 1) {
-                    count--;
-                    break;
-                }
-                right++;
-            }
-            insert(nums, left, right - 1);
-            if (nums[left] % p == 0) {
-                count--;
-            }
-            left++;
-        }
-        return totalCount;
-    }
+	public int countDistinct(int[] nums, int k, int p) {
+		int count = 0;
+		int n = nums.length;
+		int left = 0;
+		int right = 0;
+		while (left < n) {
+			while (right < n) {
+				if (nums[right] % p == 0) {
+					count++;
+				}
+				if (count == k + 1) {
+					count--;
+					break;
+				}
+				right++;
+			}
+			insert(nums, left, right - 1);
+			if (nums[left] % p == 0) {
+				count--;
+			}
+			left++;
+		}
+		return totalCount;
+	}
 }
